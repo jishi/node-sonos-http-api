199e80b8069a6e0d1a1e4296dfb1eb6382030340
Jimmy Shimizu <jimmy@shimizu.se>
2014-11-23 02:49:31 +0100

    modularized actions

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var SonosDiscovery = require('sonos-discovery');
 var SonosHttpAPI = require('./lib/sonos-http-api.js');
 var fs = require('fs');
 var path = require('path');
+var requireFu = require('require-fu');
 
 var settings = {
   port: 5005,
   cacheDir: './cache'
 }
 
+var presets = {};
 var discovery = new SonosDiscovery(settings);
+var api;
 
 try {
   var userSettings = require(path.resolve(__dirname, 'settings.json'));
@@ -23,8 +26,6 @@ if (userSettings) {
   }
 }
 
-var presets = {};
-
 fs.exists('./presets.json', function (exists) {
 	if (exists) {
 		presets = require('./presets.json');
@@ -32,6 +33,7 @@ fs.exists('./presets.json', function (exists) {
 	} else {
 		console.log('no preset file, ignoring...');
 	}
-	new SonosHttpAPI(discovery, settings, presets);
+	api = new SonosHttpAPI(discovery, settings, presets);
+	requireFu(__dirname + '/lib/actions')(api);
 });
 
