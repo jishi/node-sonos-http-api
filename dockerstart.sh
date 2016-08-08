28b998be0ae0ca93945b2e3e4ca26b18ff9d2335
ADN182 <adn62@hotmail.com>
2016-08-08 22:40:31 +0200

    Update dockerstart.sh

--- dockerstart.sh
+++ dockerstart.sh
@@ -4,7 +4,8 @@ cd /sonos
 # try to remove the repo if it already exists
 rm -rf node-sonos-http-api; true
 
-git clone https://github.com/jishi/node-sonos-http-api
+#git clone https://github.com/jishi/node-sonos-http-api
+git clone -b beta https://github.com/ADN182/node-sonos-http-api.git
 
 cd node-sonos-http-api
 
